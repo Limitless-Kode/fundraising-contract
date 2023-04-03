0f5fbaf30e148019e10c5d51a3b59b44a62151c1
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-04-03 22:15:16 +0000

    updated migration to transfer tokens to Fundraising Contract

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,11 @@
-const Helper = artifacts.require("Helper");
-const Lottery = artifacts.require("Lottery");
+const ClaverToken = artifacts.require("ClaverToken");
+const Fundraising = artifacts.require("Fundraising");
 
-module.exports = function(deployer) {
-  deployer.deploy(Helper);
-  deployer.link(Helper, Lottery);
-  deployer.deploy(Lottery);
+module.exports = async function (deployer) {
+  await deployer.deploy(ClaverToken);
+  const token = await ClaverToken.deployed();
+
+  await deployer.deploy(Fundraising);
+  const fundraising = await Fundraising.deployed();
+  await token.transfer(fundraising.address, (1000000 * 10 ** 18).toString());
 };
